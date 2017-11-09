f90a1195c612effe4013c033a70cdc43ee6924e2
Nathan Burgers <nburgers@bloomberg.net>
2017-11-08 19:58:26 -0500

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Added basic BDE metadata support for ocaml packages

--- src/mls/mls_avltree.mli
+++ src/mls/mls_avltree.mli
@@ -6,116 +6,30 @@ module type Comparable = Mls_comparable.Signature
 
 module Make (Key : Comparable) : sig
 
+    (* MODULES *)
     module Key : Comparable
 
-    (* MODULES *)
-    module rec Tree_type : sig
-        type 'value t = Branch of 'value Branch_type.t
-                      | Empty
-    end
-
-    and Branch_type : sig
-        type 'value t = {
-            height      :  int               ;
-            key         :  Key.t             ;
-            value       : 'value             ;
-            left_child  : 'value Tree_type.t ;
-            right_child : 'value Tree_type.t ;
-        }
-    end
-
-    module Tree : sig
-        type 'value t = 'value Tree_type.t
-                      = Branch of 'value Branch_type.t
-                      | Empty
-
-        val make_branch : 'value Branch_type.t -> 'value t
-        val make_empty  :  unit                -> 'value t
-    end
-
-    module Branch : sig
-        type 'value t = 'value Branch_type.t = {
-            height      :  int               ;
-            key         :  Key.t             ;
-            value       : 'value             ;
-            left_child  : 'value Tree_type.t ;
-            right_child : 'value Tree_type.t ;
-        }
-
-        val make  :  key          :  Key.t
-                 ->  value        : 'value
-                 -> ?left_child   : 'value Tree_type.t
-                 -> ?right_child  : 'value Tree_type.t
-                 -> 'value t
-
-        val height      : 'value t ->  int
-        val key         : 'value t ->  Key.t
-        val value       : 'value t -> 'value
-        val left_child  : 'value t -> 'value Tree_type.t
-        val right_child : 'value t -> 'value Tree_type.t
-    end
-
     (* TYPES *)
-    type  key          =  Key.t
-    type 'value branch = 'value Branch.t
-    type 'value t      = 'value Tree.t
+    type 'value t
 
     (* FUNCTIONS *)
-    val make :  key         :  key
-            ->  value       : 'value
-            -> ?left_child  : 'value t
-            -> ?right_child : 'value t
-            -> 'value t
-
     val make_empty : unit -> 'value t
 
     val balance : 'value t -> int
-    val height  : 'value t -> int
 
-    val is_avl         : 'value t -> bool
-    val is_balanced    : 'value t -> bool
-    val is_left_heavy  : 'value t -> bool
-    val is_right_heavy : 'value t -> bool
+    val height : 'value t -> int
 
-    val key : 'value t -> key Optional.t
-    (**
-     * Return the key of the top level node in the tree if the tree is
-     * non-empty, otherwise return nothing.
-     *)
+    val insert : 'value t
+              ->  key   :  Key.t
+              ->  value : 'value
+              -> 'value t
 
-    val value : 'value t -> 'value Optional.t
-    (**
-     * Return the value of the top level node in the tree if the tree is
-     * non-empty, otherwise return nothing.
-     *)
+    val remove : 'value t
+              ->  key : Key.t
+              -> 'value t
 
-    val left_child  : 'value t -> 'value t
-    (**
-     * Return the left child of the tree if the tree is a branch, otherwise
-     * return an empty tree.
-     *)
-
-    val right_child : 'value t -> 'value t
-    (**
-     * Return the right child of the tree if the tree is a branch, otherwise
-     * return an empty tree.
-     *)
-
-    val rebalance : 'value t -> 'value t
-    (**
-     * Given a tree with children that satisfy the AVL invariant, rebalance
-     * this tree such that it satisfies the avl invariant.  Return the value
-     * unmodified if the value is a leaf or empty.  The behavior is undefined
-     * if the value has subtrees that violate the AVL invariant.
-     *)
-
-    val insert : 'value t -> key:key -> value:'value -> 'value t
-
-    val lookup : 'value t -> key:key -> 'value Optional.t
-
-    val rotate_left       : 'value t -> 'value t
-    val rotate_right      : 'value t -> 'value t
-    val rotate_left_right : 'value t -> 'value t
-    val rotate_right_left : 'value t -> 'value t
+    val lookup : 'value t
+              ->  key : Key.t
+              -> 'value Optional.t
 
 end with module Key = Key
